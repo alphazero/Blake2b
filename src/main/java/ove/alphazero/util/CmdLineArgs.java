03d16c54fc6e6653723da9760c3ec699c0383268
Joubin Houshyar <alphazero@sensesay.net>
2014-02-14 14:25:52 -0500

    ADD a little benchmark tool to compare with other digests.
       - to run:
         java -cp target/classes ove.crypto.digest.Bench [options]
       - note: run with no cmdline args to get the usage info.

---
+++ src/main/java/ove/alphazero/util/CmdLineArgs.java
@@ -0,0 +1,122 @@
+/* !!! Doost !!! */
+
+/*
+   A Java implementation of BLAKE2B cryptographic digest algorithm.
+
+   Joubin Mohammad Houshyar <alphazero@sensesay.net>
+   bushwick, nyc
+   02-14-2014
+
+   --
+
+   To the extent possible under law, the author(s) have dedicated all copyright
+   and related and neighboring rights to this software to the public domain
+   worldwide. This software is distributed without any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication along with
+   this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+package ove.alphazero.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class CmdLineArgs {
+	static final String PREFIX_FLAG = "--";
+	static final String PREFIX_OPT = "-";
+	/** */
+	public interface Spec { /* nop for now */}
+
+	/** */
+	final Map<String, String> map;
+	final Spec spec;
+	/** */
+	private CmdLineArgs (final Spec spec, final Map<String, String> map){
+		assert map != null : "map is null";
+		this.spec = spec;
+		this.map = map;
+	}
+	/** */
+	public boolean checkFlag(String f) {
+		return map.containsKey(flagKey(f));
+	}
+	/** */
+	public String getOption(String k, final String defval) {
+		String v = map.get(optKey(k));
+		if(v == null) {
+			v = defval;
+		}
+		return v;
+	}
+	/** */
+	public String getOptionStrict(String k) {
+		return assertNotNull(map.get(optKey(k)), "must specify required option " + k);
+	}
+	/** */
+	public int getIntOption(String k, int defval) {
+		int res = defval;
+		final String optv = getOption(k, null);
+		if(optv != null) {
+			res = decodeInt(optv, "invalid value for numeric option " + k);
+		}
+		return res;
+	}
+	/** */
+	public String getparam(int n) {
+		return map.get(paramKey(n));
+	}
+	private static String paramKey(final int n) { return String.format("$%d", n);}
+	private static String flagKey(final String f) { return String.format("%s%s", PREFIX_FLAG, f);}
+	private static String optKey(final String o) { return String.format("%s%s", PREFIX_OPT, o);}
+	/** */
+	public static CmdLineArgs parse(final Spec spec, String...args) {
+		if(args == null) return null;
+
+		final Map<String, String> map = new HashMap<String, String>();
+		int fn = 0;
+		int on = 0;
+		int pn = 0;
+		for(int i=0; i<args.length; i++) {
+			String k = null;
+			String v = null;
+			if ( args[i] == null ) continue;
+			if ( args[i].startsWith (PREFIX_FLAG) ) {
+				k = args[i];
+				v = k;
+				fn++;
+			} else if( args[i].startsWith ( PREFIX_OPT ) ) {
+				assert0 (args.length-1 > i, String.format("no value for arg %s at args end", args[i]));
+				assert0 (!args[i+1].startsWith(PREFIX_OPT), String.format("no value for arg %s", args[i]));
+				assert0 (!args[i+1].startsWith(PREFIX_FLAG), String.format("no value for arg %s", args[i]));
+				k = args[i];
+				i++;
+				v = args[i];
+				on++;
+			} else {
+				pn++;
+				k = paramKey(pn);
+				v = args[i];
+			}
+			map.put(k, v);
+		}
+		return new CmdLineArgs(spec, map);
+	}
+
+	public static void assert0 (final boolean prop, final String errmsg) throws IllegalStateException{
+		if (!prop) throw new IllegalStateException(errmsg);
+	}
+	public static <T> T assertNotNull (final T o, final String errmsg) throws IllegalStateException{
+		if (o == null) throw new IllegalStateException(errmsg);
+		return o;
+	}
+	public static int decodeInt(final String spec, final String errmsg) throws IllegalArgumentException {
+		int v;
+		try{
+			v = Integer.parseInt(spec);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(String.format(errmsg, spec));
+		}
+		return v;
+	}
+}
---
+++ src/main/java/ove/crypto/digest/Bench.java
@@ -0,0 +1,154 @@
+/* !!! Doost !!! */
+
+/*
+   A Java implementation of BLAKE2B cryptographic digest algorithm.
+
+   Joubin Mohammad Houshyar <alphazero@sensesay.net>
+   bushwick, nyc
+   02-14-2014
+
+   --
+
+   To the extent possible under law, the author(s) have dedicated all copyright
+   and related and neighboring rights to this software to the public domain
+   worldwide. This software is distributed without any warranty.
+
+   You should have received a copy of the CC0 Public Domain Dedication along with
+   this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+package ove.crypto.digest;
+
+import ove.alphazero.util.CmdLineArgs;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+public class Bench implements Runnable {
+
+	interface Default {
+		int iterations = 1000;
+		int datalen = 1024;
+		String digest = "blake2b";
+	}
+
+	static volatile boolean f_run = true;
+
+	static class Usage {
+		private static void explain (final String opt, final String details) {
+			System.out.format("%3s\t%s\n", opt, details);
+		}
+		static int usage () {
+			System.out.println("usage: java -cp .. ove.crypto.digest.Bench [options]");
+			System.out.println("[options]");
+			explain ("-d",  "digest to bench, one of {blake2, sha1, md5}. default: blake2b");
+			explain ("-i",  "number of iterations (digest function calls) per bench round. default: 1000");
+			explain ("-n",  "size of the digested buffer in bytes. default: 1024 b / call");
+
+			return -1;
+		}
+	}
+	public static void main(final String... args) throws Exception{
+		if(args.length == 0) {
+			System.exit(Usage.usage());
+		}
+		final CmdLineArgs clargs = CmdLineArgs.parse(null, args);
+		try {
+			String md_name  = null;
+			int iters;
+			int datalen;
+
+			md_name = clargs.getOption("d", Default.digest);
+			iters = clargs.getIntOption("i", Default.iterations);
+			datalen = clargs.getIntOption("n", Default.datalen);
+
+			final Bench bench = new Bench (md_name, iters, datalen);
+			final Thread brth = new Thread(bench, "bench-runner");
+			brth.start();
+			System.in.read();
+			f_run = false;
+			brth.join();
+
+		} catch (Throwable e) {
+			System.exit(Usage.usage());
+		}
+
+	}
+
+	private final String md_name;
+	private final int iters;
+	private final int datalen;
+	private final byte[] b;
+	private final Call call;
+
+	Bench (final String md_name, final int iters, final int datalen) {
+		this.md_name = md_name;
+		this.iters = iters;
+		this.datalen = datalen;
+		this.b = new byte[datalen];
+		for(int i=0; i<b.length; i++) {
+			b[i] = (byte)i;
+		}
+		this.call = getBenchedCall();
+	}
+
+	private Call getBenchedCall() {
+
+		Call call = null;
+		if (md_name.equalsIgnoreCase("blake2b")) {
+			call = newCallBlake2b();
+		} else {
+			call = newCallJCEAlgorithm(md_name);
+		}
+		return call;
+	}
+	private static final void puts(final String s) {
+		System.out.format("%s\n", s);
+	}
+	@Override public void run () {
+		puts ("Bench - hit any key to stop.");
+		puts ("");
+		puts ("digest   | iterations | size (b/iter) | dt (nsec/iter) | throughput (b/usec)");
+		while (f_run) {
+			final long start = System.nanoTime();
+			for(int i=0; i<iters; i++)
+				call.func(b);
+			final long delta = System.nanoTime() - start;
+
+			final long delta_us = TimeUnit.NANOSECONDS.toMicros(delta);
+			final double thrpt = ((double) b.length * iters) / delta_us;
+			System.out.format("%-8s | %10d | %13d | %14d |    %16.6f\r", md_name, iters, b.length, delta, thrpt);
+		}
+	}
+	interface Call {
+		byte[] func(final byte[] b);
+	}
+
+	public static Call newCallBlake2b ()  {
+		final Blake2b digest = Blake2b.Digest.newInstance (new Blake2b.Param().setDigestLength(20));
+		return new Call() {
+			@Override final public byte[] func(byte[] b) {
+				digest.update(b, 0, b.length);
+				return digest.digest();
+			}
+		};
+	}
+
+	public static Call newCallJCEAlgorithm (final String md_name) {
+		final MessageDigest digest = silentGet (md_name);
+		return new Call() {
+			@Override final public byte[] func(byte[] b) {
+				digest.update(b, 0, b.length);
+				return digest.digest();
+			}
+		};
+	}
+	public static MessageDigest silentGet (final String mdname) {
+		try {
+			return MessageDigest.getInstance(mdname);
+		} catch (Throwable e) {
+			throw new Error (String.format("Error getting instance of digest <%s>", mdname), e);
+		}
+	}
+}
